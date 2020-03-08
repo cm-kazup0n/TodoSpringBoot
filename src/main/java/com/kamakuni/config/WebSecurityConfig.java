8444678e212f4c7d6d22a7d807997a4870a12ca1
kamakuni <superradio86@gmail.com>
2020-03-08 21:58:39 +0900

    add LoginController

--- src/main/java/com/kamakuni/config/WebSecurityConfig.java
+++ src/main/java/com/kamakuni/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.kamakuni.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,20 +9,20 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/login").permitAll()
-		.anyRequest().authenticated()
-		.and()
+			.antMatchers("/login").permitAll()
+			.anyRequest().authenticated()
+			.and()
 		.formLogin()
-		.loginPage("/login")
-		.loginProcessingUrl("/authenticate")
-		.defaultSuccessUrl("/todos")
-		.failureUrl("/login")
-		.usernameParameter("username")
+			.loginPage("/login")
+			.loginProcessingUrl("/authenticate")
+			.defaultSuccessUrl("/todos")
+			.failureUrl("/login")
+		.usernameParameter("email")
 		.passwordParameter("password")
 		.and()
 		.logout()
@@ -30,5 +31,15 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 		.deleteCookies("JSESSIONID")
 		.invalidateHttpSession(true)
 		.permitAll();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("email").password("{noop}password").roles("USER")
+		.and()
+			.withUser("admin").password("{noop}admin").roles("ADMIN");
+			//.credentialsExpired(true).accountExpired(true).accountLocked(true)
+			//.authorities("WRITE_PRIVILEGES", "READ_PRIVILEGES").roles("ADMIN");
 	}
 }
