d8c07e2006a1b12ebb4d5f652c3a386a6f118304
kamakuni <superradio86@gmail.com>
2020-03-10 23:25:45 +0900

    add testing codes for TodoController

--- src/test/java/com/kamakuni/controller/TodoControllerTest.java
+++ src/test/java/com/kamakuni/controller/TodoControllerTest.java
@@ -8,8 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.kamakuni.service.TodoService;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(TodoController.class)
@@ -18,7 +22,11 @@ public class TodoControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 	
+	@MockBean
+	private TodoService todoService;
+	
 	@Test
+	@WithMockUser
 	public void shouldReturnTodoView() throws Exception {
 		this.mockMvc.perform(get("/todos"))
 			.andExpect(view().name("todos/index"))
