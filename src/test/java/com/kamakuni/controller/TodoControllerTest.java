3f74d7d5bf30e8f99fecf29f4c3920ac2956ba21
kamakuni <superradio86@gmail.com>
2020-03-11 21:32:15 +0900

    add testing codes for TodoController.edit and delete

--- src/test/java/com/kamakuni/controller/TodoControllerTest.java
+++ src/test/java/com/kamakuni/controller/TodoControllerTest.java
@@ -1,8 +1,12 @@
 package com.kamakuni.controller;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.Date;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +17,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.kamakuni.entity.Todo;
 import com.kamakuni.service.TodoService;
 
 @ExtendWith(SpringExtension.class)
@@ -38,6 +43,24 @@ public class TodoControllerTest {
 	public void shouldReturnNewView() throws Exception {
 		this.mockMvc.perform(get("/todos/new"))
 			.andExpect(view().name("todos/new"))
+			.andExpect(status().isOk());
+	}
+	
+	@Test
+	@WithMockUser
+	public void shouldReturnEditView() throws Exception {
+		when(todoService.findOne(1L)).thenReturn(Optional.of(new Todo(1L,"title",false, new Date(), new Date())));
+		this.mockMvc.perform(get("/todos/1/edit"))
+			.andExpect(view().name("todos/edit"))
+			.andExpect(status().isOk());
+	}
+
+	@Test
+	@WithMockUser
+	public void shouldReturnDeleteView() throws Exception {
+		when(todoService.findOne(1L)).thenReturn(Optional.of(new Todo(1L,"title",false, new Date(), new Date())));
+		this.mockMvc.perform(get("/todos/1/delete"))
+			.andExpect(view().name("todos/delete"))
 			.andExpect(status().isOk());
 	}
 
